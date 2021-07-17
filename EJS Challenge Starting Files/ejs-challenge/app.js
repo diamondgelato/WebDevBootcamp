2cc26de68e0936014461af78e8addb01327c42cd
Mugdha Kurkure <60062557+diamondgelato@users.noreply.github.com>
2021-07-18 03:05:21 +0530

    Database connection to Blog website complete

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -3,12 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
+const mongoose = require("mongoose");
+// let posts = [];
 
 const app = express();
 
@@ -20,11 +16,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+DBConnect ();
 
+// Posts schema
+const postSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  body: String
+});
+
+// Posts model
+const Post = mongoose.model ("Post", postSchema);
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+// Home page: shows all the post previews
 app.get ("/", function (req, res) {
-  res.render ("home", {
-    homeContent: homeStartingContent, 
-    posts: posts});
+  Post.find ((err, posts) => {
+    if (err) {
+      console.log ("There was some error :(")
+    } else if (posts) {
+      res.render ("home", {
+        homeContent: homeStartingContent, 
+        posts: posts
+      });
+    }
+  })
 })
 
 app.get ("/about", function (req, res) {
@@ -35,34 +56,46 @@ app.get ("/contact", function (req, res) {
   res.render ("contact", {contactContent: contactContent});
 })
 
+// Compose: Make new posts here
 app.get ("/compose", function (req, res) {
   res.render ("compose");
 })
 
-app.get ("/posts/:postName", function (req, res) {
+// Post page: Shows specified post in full
+app.get ("/posts/:postID", function (req, res) {
   let isFound = 0;
-
-  posts.forEach (function (post) {
-    if (_.lowerCase(post.blogTitle) == _.lowerCase(req.params.postName)) {
+  
+  // update to be compliant with mongoose
+  Post.findOne (
+    // {name: _.lowerCase(req.params.postID)},
+    {_id: req.params.postID},
+    (err, post) => {
+    // if (_.lowerCase(post.blogTitle) == _.lowerCase(req.params.postID)) {
       console.log ("match found!");
+      // console.log (post)
       res.render ("post", {post: post});
       isFound = 1;
-    }
+    // }
   });
 
   if (isFound == 0) {
     console.log("not a match");
   }
-  // console.log (req.params.postName)
+  // console.log (req.params.postID)
 })
 
-app.post ("/compose", function (req, res) {
-  let post = {
-    blogTitle: req.body.blogTitle,
-    blogContent: req.body.blogContent 
-  };
 
-  posts.push (post);
+
+// Post-compose: page loaded after submit button on compose page is pressed
+app.post ("/compose", function (req, res) {
+  let newPost = new Post ({
+    name: req.body.blogTitle,
+    body: req.body.blogContent 
+  });
+
+  newPost.save ();
+
+  // posts.push (post);
 
   // console.log(posts);
   res.redirect ("/");
@@ -73,3 +106,14 @@ app.post ("/compose", function (req, res) {
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+
+async function DBConnect () {
+  try {
+    await mongoose.connect ('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log ("Database Connected");
+  } catch (error) {
+    console.log ("Database: Error in Connection");
+  }
+}
